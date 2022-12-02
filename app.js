4a2b88a30d38e7c3b0729b593835955b141fbcc6
leticea <nascimento.leticeah@gmail.com>
2022-12-02 19:01:53 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const humanExpressions = [
+
+    { img: './img/drink.jpg', text: 'Estou com sede' },
+    { img: './img/food.jpg', text: 'Estou com fome' },
+    { img: './img/tired.jpg', text: 'Estou cansado' },
+    { img: './img/hurt.jpg', text: 'Estou machudada' },
+    { img: './img/happy.jpg', text: 'Estou feliz' },
+    { img: './img/angry.jpg', text: 'Estou com raiva' },
+    { img: './img/sad.jpg', text: 'Estou triste' },
+    { img: './img/scared.jpg', text: 'Estou assustada' },
+    { img: './img/outside.jpg', text: 'Quero ir lá fora' },
+    { img: './img/home.jpg', text: 'Quero ir para casa' },
+    { img: './img/school.jpg', text: 'Quero ir para a escola' },
+    { img: './img/grandma.jpg', text: 'Quero ver a vovó' },
+];
+
